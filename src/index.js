927b58d5bfcf6d451775134bf5d0a74cd1147856
cjayasanka <cjayasanka93@gmail.com>
2019-09-14 11:00:13 +0530

    adding css

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  // constructor (props){
+  //     super(props);
 
-    constructor (props){
-        super(props);
+  //     this.state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
+  // }
 
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.lat} />
+        </div>
+      );
     }
 
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude}),
-            (err) => this.setState({ errorMessage: err.message})
-        );
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
-    render(){
-        if (this.state.lat && !this.state.errorMessage){
-            return <div>Latitude: {this.state.lat}</div>;
-        }
-
-        if (!this.state.lat && this.state.errorMessage){
-            return <div>Error: {this.state.errorMessage}</div>;
-        }
-
-        return <div>Loading!</div>;
-    };
+    return <div>Loading!</div>;
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
